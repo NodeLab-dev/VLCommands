7ebbbaff05c5b0cbe4f07a292e52205387a1967b
VOLTLab <76522471+VOLTLab1@users.noreply.github.com>
2020-12-25 06:39:57 +0000

    Delete client.lua

--- client/client.lua
+++
@@ -1,7 +0,0 @@
-RegisterCommand('help', function()
-    TriggerEvent('chat:addMessage', {
-        color = {255,0,0},
-        multiline = true,
-        args = {'[SERVER] :', ' Join the discord for help, discord.gg/LINK'}
-    })
-end) -- /help
